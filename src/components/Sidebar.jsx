80aae75f8ba81adf21c0dff8a4061116d48ab1f1
givenchy <johnsonemmanuel1425@gmail.com>
2026-05-04 13:59:42 +0300

    Added injection tracking for nurses

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useTheme } from '../context/ThemeContext.jsx';
+// import { useTheme } from '../context/ThemeContext.jsx';
 import { 
   Home, 
   Users, 
